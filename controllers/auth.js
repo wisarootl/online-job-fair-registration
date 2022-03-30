8e9da9079e4c17b797a47bc7c096476872a35d8a
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2022-03-30 22:09:12 +0700

    debugging abd test with postman for resgistration/login/logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,70 +1,81 @@
-const res = require('express/lib/response');
-const User = require('../models/User');
+const res = require('express/lib/response')
+const User = require('../models/User')
 
-//@desc Register user
-//@route POST /api/v1/auth/register
-//@access public
-exports.register = async (req,res,next)=>{
-   try{
-        const {name,email,password,role} = req.body;  //Create user  // แจง .body เป็น 4 ค่า
-        const user = await User.create({
-            name,
-            telephoneNumber,
-            email,
-            password,
-            role
-        });
-        sendTokenResponse(user,200,res);        // Create token
-    } catch(err){
-        res.status(400).json({success:false});
-        console.log(err.stack);
-    }
+//@desc     Register user
+//@route    POST /api/v1/auth/register
+//@access   Public
+exports.register = async (req, res, next) => {
+  try {
+    // * add telephone numbers
+    const { name, email, password, telephoneNumber, role } = req.body //Create user  // แจง .body เป็น 4 ค่า
+    const user = await User.create({
+      name,
+      telephoneNumber,
+      email,
+      password,
+      role
+    })
+    console.log(user)
+    sendTokenResponse(user, 200, res) // Create token
+  } catch (err) {
+    // * add return message for error case
+    res.status(400).json({ success: false, msg: err['message'] })
+    console.log(err)
+  }
 }
 
-//@desc Login user
-//@route POST /api/v1/auth/login
-//@access public
-exports.login = async (req,res,next)=>{
-    const{email,password} = req.body;
-    if(!email || !password){        // Verify username,password
-        return res.status(400).json({success:false,msg:'Please provide an email and password'});
-    }
-    const user = await User.findOne({email}).select('+password');   //Check for user
-    if(!user){
-        return res.status(400).json({success:false,msg:'Invalid credentials'});
-    }
-    const isMatch = await user.matchPassword(password);     //Check if password matches
-    if(!isMatch){
-        return res.status(401).json({success:false,msg:'Invalid credentials'});
-    }
-    sendTokenResponse(user,200,res);    //Create token
-};
+//@desc     Login user
+//@route    POST /api/v1/auth/login
+//@access   Public
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    // Verify username, password
+    return res.status(400).json({ success: false, msg: 'Please provide an email and password' })
+  }
 
-//At the end of file
+  const user = await User.findOne({ email }).select('+password') //Check for user
+  if (!user) {
+    return res.status(400).json({ success: false, msg: 'Invalid credentials' })
+  }
+
+  const isMatch = await user.matchPassword(password) //Check if password matches
+  if (!isMatch) {
+    return res.status(401).json({ success: false, msg: 'Invalid credentials' })
+  }
+
+  sendTokenResponse(user, 200, res) //Create token
+}
+
 //@desc     Get current Logged in user
 //@route    POST /api/v1/auth/me
 //@access   Private
-exports.getMe = async (req,res,next)=>{
-    const user = await User.findById(req.user.id);
-    let role=req.user.role;
-    res.status(200).json({success:true,status:role,data:user});
-};
+exports.getMe = async (req, res, next) => {
+  const user = await User.findById(req.user.id)
+  let role = req.user.role
+  res.status(200).json({ success: true, status: role, data: user })
+}
 
-exports.logout = async(req, res, next)=>{
-    res.cookie('token','none',{expires: new Date(Date.now()+10*1000),httpOnly: true});
-    res.status(200).json({success:true,data:{}});
-};
+// * add log out feature
+//@desc     log out
+//@route    GET /api/v1/auth/logout
+//@access   Private
+exports.logout = async (req, res, next) => {
+  //   log out at client side
+  res.status(200).cookie('token', 'none').json({ success: true, data: {} })
+}
 
 //Get token from model, create cookie and send response
-const sendTokenResponse = (user,statusCode,res)=>{
-    //Create token
-    const token = user.getSignedJwtToken();
-    const options = {
-        expires:new Date(Date.now()+process.env.JWT_COOKIE_EXPIRE*24*60*60*1000),httpOnly:true
-    };      // แปลง day ให้กลายเป็น millisec
-    
-    if(process.env.NODE_ENV === 'production'){
-        options.secure = true;
-    }
-    res.status(statusCode).cookie('token',token,options).json({success:true,token});
+const sendTokenResponse = (user, statusCode, res) => {
+  //Create token
+  const token = user.getSignedJwtToken()
+  const options = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    httpOnly: true
+  } // แปลง day ให้กลายเป็น millisec
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true
+  }
+  res.status(statusCode).cookie('token', token, options).json({ success: true, token })
 }
--- controllers/companies.js
+++ controllers/companies.js
@@ -1,108 +1,114 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router()
-const app=express();
-const Company = require('../models/Company');
+const app = express()
+const Company = require('../models/Company')
 
 //@desc     Get all companies
 //@route    GET /api/v1/companies
 //@access   public
-exports.getCompanies = async (req,res,next)=>{
-    try{
-        let query;
-        const reqQuery = {...req.query};            // Copy req.query   // ... เพื่อแตก keyvalue จาก string เป็น array
-        const removeFields = ['select','sort','page','limit'];     // Fields to exclude    // 
-        removeFields.forEach(param=>delete reqQuery[param]);    // Loop over remove fields and delete them from reqQuery
-        console.log(reqQuery);
-        let queryStr=JSON.stringify(reqQuery);      // แปลงกลับมาเป็น String
-        queryStr=queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match=>`$${match}`);   // create operators ($gt, $$gte, etc)
-        // ค้นหาตาม pattern เพื่อแก้  // \b คือ space  // ให้ใส่ $ ข้างหน้า
-        query=Company.find(JSON.parse(queryStr)).populate('bookings');
-        // parse ให้กลับเป็น JSON // finding query // แล้วเก็บไว้ใน query
+exports.getCompanies = async (req, res, next) => {
+  try {
+    let query
+    const reqQuery = { ...req.query } // Copy req.query   // ... เพื่อแตก keyvalue จาก string เป็น array
+    const removeFields = ['select', 'sort', 'page', 'limit'] // Fields to exclude    //
+    removeFields.forEach((param) => delete reqQuery[param]) // Loop over remove fields and delete them from reqQuery
+    console.log(reqQuery)
+    let queryStr = JSON.stringify(reqQuery) // แปลงกลับมาเป็น String
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`) // create operators ($gt, $$gte, etc)
+    // ค้นหาตาม pattern เพื่อแก้  // \b คือ space  // ให้ใส่ $ ข้างหน้า
+    query = Company.find(JSON.parse(queryStr)).populate('bookings')
+    // parse ให้กลับเป็น JSON // finding query // แล้วเก็บไว้ใน query
     // Select Fields
-        if(req.query.select){
-            const fields=req.query.select.split(',').join(' ');     // แก้ ',' เป็น ' ' เพื่อให้ตรงกับ syntax ของ noSQL   // เก็บค่าไว้ใน fields
-            query=query.select(fields);       // หลักจากเลือก >,< ก็มาเลือกตาม fields อีกครั้งหนึ่ง
-        }
+    if (req.query.select) {
+      const fields = req.query.select.split(',').join(' ') // แก้ ',' เป็น ' ' เพื่อให้ตรงกับ syntax ของ noSQL   // เก็บค่าไว้ใน fields
+      query = query.select(fields) // หลักจากเลือก >,< ก็มาเลือกตาม fields อีกครั้งหนึ่ง
+    }
+
     // Sort
-        if(req.query.sort){         // เช็คว่าส่ง sort มาไหม
-            const sortBy=req.query.sort.split(',').join(' ');   // เก็บไว้ใน sortBy
-            query=query.sort(sortBy);                   // sort ตาม sortBy
-        } else{
-            query=query.sort('-createdAt');             // ถ้าไม่ได้ส่ง sort มาก็ให้เรียงตาม createAt ซึ่งคือวันที่สร้างข้อมูลขึ้นมา
-        }
+    if (req.query.sort) {
+      // เช็คว่าส่ง sort มาไหม
+      const sortBy = req.query.sort.split(',').join(' ') // เก็บไว้ใน sortBy
+      query = query.sort(sortBy) // sort ตาม sortBy
+    } else {
+      query = query.sort('-createdAt') // ถ้าไม่ได้ส่ง sort มาก็ให้เรียงตาม createAt ซึ่งคือวันที่สร้างข้อมูลขึ้นมา
+    }
     // Pagination
-        const page = parseInt(req.query.page,10)||1;
-        const limit = parseInt(req.query.limit,10)||25;
-        const startIndex=(page-1)*limit;
-        const endIndex=page*limit;
-        const total = await Hospital.countDocuments();
-        query=query.skip(startIndex).limit(limit);
+    const page = parseInt(req.query.page, 10) || 1
+    const limit = parseInt(req.query.limit, 10) || 25
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const total = await Hospital.countDocuments()
+    query = query.skip(startIndex).limit(limit)
+
     // Executing query
-        const hospitals = await query;
+    const hospitals = await query
     // Pagination result
-        const pagination = {};
-        if(endIndex<total) pagination.next={page:page+1,limit}
-        if(startIndex>0) pagination.prev={page:page-1,limit}
-        res.status(200).json({success:true,count:Companies.length, pagination, data:Companies});
-    }
-    catch(err){
-        res.status(400).json({success:false});
-    }
-};
+    const pagination = {}
+    if (endIndex < total) pagination.next = { page: page + 1, limit }
+    if (startIndex > 0) pagination.prev = { page: page - 1, limit }
+
+    res.status(200).json({ success: true, count: Companies.length, pagination, data: Companies })
+  } catch (err) {
+    res.status(400).json({ success: false })
+  }
+}
 
 //@desc     Get single company
 //@route    GET /api/v1/companies/:id
 //@access   public
-exports.getCompany = async (req,res,next)=>{
-    console.log(req.body);
-    try{
-        const company = await Company.findById(req.params.id);
-        if(!company) return res.status(400).json({success:false});
-        res.status(200).json({success:true,data:company});
-    }
-    catch(err){
-        res.status(400).json({success:false});
-    } 
-};
+exports.getCompany = async (req, res, next) => {
+  console.log(req.body)
+  try {
+    const company = await Company.findById(req.params.id)
+    if (!company) return res.status(400).json({ success: false })
+    res.status(200).json({ success: true, data: company })
+  } catch (err) {
+    res.status(400).json({ success: false })
+  }
+}
 
 //@desc     Create a company
 //@route    POST /api/v1/companies
 //@access   private
-exports.createCompany = async (req,res,next)=>{
-    console.log(req.body);
-    const company = await Company.create(req.body);
-    res.status(201).json({success:true,data:company});
-};
+exports.createCompany = async (req, res, next) => {
+  console.log(req.body)
+  const company = await Company.create(req.body)
+  res.status(201).json({ success: true, data: company })
+}
 
 //@desc     Update single companies
 //@route    PUT /api/v1/companies/:id
 //@access   private
-exports.updateCompany = async (req,res,next)=>{
-    console.log(req.body);
-    try{
-        const company = await Company.findByIdAndUpdate(req.params.id,req.body,{new: true,runValidators:true});
-        if(!company) return res.status(400).json({success:false});
-        res.status(200).json({success:true,data:company});
-    }
-    catch(err){
-        res.status(400).json({success:false});
-    }
-};
+exports.updateCompany = async (req, res, next) => {
+  console.log(req.body)
+  try {
+    const company = await Company.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    })
+    if (!company) return res.status(400).json({ success: false })
+    res.status(200).json({ success: true, data: company })
+  } catch (err) {
+    res.status(400).json({ success: false })
+  }
+}
 
 //@desc     Delete single companies
 //@route    DELETE /api/v1/companies/:id
 //@access   private
-exports.deleteCompany = async (req,res,next)=>{
-    console.log(req.body);
-    try{
-        const company = await Company.findById(req.params.id);
-        if(!company) return res.status(404).json({success:false,message:`Bootcamp not found with id of ${req.params.id}`});
-        company.remove();
-        res.status(200).json({success:true,data:{}});
-    }
-    catch(err){
-        res.status(400).json({success:false});
-    }
-};
+exports.deleteCompany = async (req, res, next) => {
+  console.log(req.body)
+  try {
+    const company = await Company.findById(req.params.id)
+    if (!company)
+      return res
+        .status(404)
+        .json({ success: false, message: `Bootcamp not found with id of ${req.params.id}` })
+    company.remove()
+    res.status(200).json({ success: true, data: {} })
+  } catch (err) {
+    res.status(400).json({ success: false })
+  }
+}
 
-module.exports=exports;
+module.exports = exports
