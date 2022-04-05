03f71b2db813d1d27a644566441e09ac63b85ef6
jjulanan <kop.julanan@gmail.com>
2022-04-05 17:38:23 +0700

    Install Security Package

--- models/User.js
+++ models/User.js
@@ -3,45 +3,6 @@ const bcrypt = require('bcryptjs') // npm i jsonwebtoken bcryptjs
 const jwt = require('jsonwebtoken') // npm i jsonwebtoken bcryptjs
 
 const UserSchema = new mongoose.Schema({
-<<<<<<< HEAD
-  name: { type: String, required: [true, 'Please submit your name'] },
-  telephoneNumber: {
-    type: String,
-    required: [true, 'Please submit your telephone number']
-  },
-  email: {
-    type: String,
-    required: [true, 'Please submit your email'],
-    unique: true,
-    match: [
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      'Please add a valid email'
-    ]
-  },
-  password: {
-    type: String,
-    required: [true, 'Please set your password'],
-    minlength: 6,
-    select: false
-  },
-  resetPasswordToken: String,
-  resetPasswordExpire: Date,
-  role: { type: String, enum: ['user', 'admin'], default: 'user' },
-  createdAt: { type: Date, default: Date.now }
-})
-
-// encrypt password using bcrypt
-UserSchema.pre('save', async function (next) {
-  const salt = await bcrypt.genSalt(10)
-  this.password = await bcrypt.hash(this.password, salt)
-})
-
-// sign JWT and return
-UserSchema.methods.getSignedJwtToken = function () {
-  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE
-  })
-=======
     name:{
         type:String,
         required:[true,"Please submit your name"]
@@ -66,6 +27,7 @@ UserSchema.methods.getSignedJwtToken = function () {
     role:{type:String,enum:["user","admin"],default:"user"},
     createdAt:{type:Date,default:Date.now}
 });
+
 //Encrypt password using bcrypt
 UserSchema.pre('save',async function(next){
     const salt=await bcrypt.genSalt(10);
@@ -77,12 +39,6 @@ UserSchema.methods.getSignedJwtToken=function(){
         expiresIn:process.env.JWT_EXPIRE
     });
 }
-//Match user entered password to hashed pasword in database
-UserSchema.methods.matchPassword = async function(enteredPassword){
-    return await bcrypt.compare(enteredPassword, this.password);
->>>>>>> origin
-}
-
 // match user entered password to hashed pasword in database
 UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
