932bce5c0d7bfbf727e18d7a974de76d975897d9
jjulanan <kop.julanan@gmail.com>
2022-04-10 01:39:21 +0700

    pending bug fix, dup email+tel

--- config/db.js
+++ config/db.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async()=> {
     const conn= await mongoose.connect(process.env.MONGO_URI,{
         useNewUrlParser: true,
-        useUnifiedTopology: true});
+        useUnifiedTopology: true,
+        autoIndex: true     // fix "unique: true not enforced" bug
+    });            
     console.log(`MongoDB Connected:${conn.connection.host}`);
 }
 
